ecdae4884cb3b57ab40bcf70d70387cd390e28df
Dan Carley <dan.carley@gmail.com>
2012-04-25 08:50:07 +0100

    ddns classes depend on ec2_public_hostname fact.

--- manifests/ddns.pp
+++ manifests/ddns.pp
@@ -1,7 +1,7 @@
 class ec2init::ddns {
     include ::ec2init::params
 
-    if $::ec2init::params::hostname and $::ec2init::params::aws_key and $::ec2init::params::aws_secret {
+    if $::ec2init::params::hostname and $::ec2init::params::aws_key and $::ec2init::params::aws_secret and $::ec2_public_hostname {
         package {
             'python-boto':
                 ensure  => present;
--- manifests/ddns/stop.pp
+++ manifests/ddns/stop.pp
@@ -1,7 +1,7 @@
 class ec2init::ddns::stop inherits ec2init::ddns {
     include ::ec2init::params
 
-    if $::ec2init::params::hostname and $::ec2init::params::aws_key and $::ec2init::params::aws_secret {
+    if $::ec2init::params::hostname and $::ec2init::params::aws_key and $::ec2init::params::aws_secret and $::ec2_public_hostname {
         Exec['register dynamic dns hostname'] {
             command => "/usr/bin/python /usr/sbin/ec2ddns.py -k ${::ec2init::params::aws_key} -s ${::ec2init::params::aws_secret} ${::ec2init::params::hostname} --delete",
         }
