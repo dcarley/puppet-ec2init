b0bda61b927b9d405f8d27a78898e8fd58bfa289
Dan Carley <dan.carley@gmail.com>
2012-04-28 08:58:39 +0100

    Adapt has_key tests for rspec-puppet.

--- spec/functions/has_key_spec.rb
+++ spec/functions/has_key_spec.rb
@@ -1,5 +1,6 @@
 # Copyright (C) 2011 Puppet Labs Inc
 # Copyright (C) 2011 Krzysztof Wilczynski
+# Copyright (C) 2012 Dan Carley
 # 
 # Puppet Labs can be contacted at: info@puppetlabs.com
 # 
@@ -15,52 +16,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Borrowed from puppetlabs-stdlib
+# Adapted from puppetlabs-stdlib for rspec-puppet.
 # https://github.com/puppetlabs/puppetlabs-stdlib
 
-require 'puppet'
-require 'mocha'
-describe Puppet::Parser::Functions.function(:has_key) do
+require 'spec_helper'
 
-  # Pulled from Dan's create_resources function
-  # TODO - this should be moved to spec_helper since the
-  # logic is likely to be applied to multiple rspec files.
-  let(:compiler) {
-    topscope = Puppet::Parser::Scope.new
-    # This is necessary so we don't try to use the compiler to discover our parent.
-    topscope.parent = nil
-    my_scope = Puppet::Parser::Scope.new
-    my_scope.compiler = Puppet::Parser::Compiler.new(Puppet::Node.new("floppy", :environment => 'production'))
-    my_scope.parent = topscope
-    compiler = my_scope.compiler
-  }
-  let(:scope) {
-    scope = Puppet::Parser::Scope.new
-    scope.stubs(:environment).returns(Puppet::Node::Environment.new('production'))
-    scope
-  }
-
+describe 'has_key' do
   describe 'when calling has_key from puppet' do
     it "should not compile when no arguments are passed" do
-      Puppet[:code] = 'has_key()'
-      expect { compiler.compile }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
+      expect { subject.call([]) }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
     end
     it "should not compile when 1 argument is passed" do
-      Puppet[:code] = "has_key('foo')"
-      expect { compiler.compile }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
+      expect { subject.call([{'foo' => 'bar'}]) }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
     end
     it "should require the first value to be a Hash" do
-      Puppet[:code] = "has_key('foo', 'bar')"
-      expect { compiler.compile }.should raise_error(Puppet::ParseError, /expects the first argument to be a hash/)
+      expect { subject.call(['foo', 'bar']) }.should raise_error(Puppet::ParseError, /expects the first argument to be a hash/)
     end
   end
   describe 'when calling the function has_key from a scope instance' do
     it 'should detect existing keys' do
-      scope.function_has_key([{'one' => 1}, 'one']).should be_true
+      subject.call([{'one' => 1}, 'one']).should be_true
     end
     it 'should detect existing keys' do
-      scope.function_has_key([{'one' => 1}, 'two']).should be_false
+      subject.call([{'one' => 1}, 'two']).should be_false
     end
   end
-
 end
