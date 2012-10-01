1f7e03d04b7066987b55bf5cf0a761059c517ffd
David Swift <dswift@tdl.com>
2012-10-01 16:30:03 -0400

    add the aws_creds.sh file into /etc/profile.d to have shell functions access the AWS keys as they are updated.

---
+++ aws_creds.sh
@@ -0,0 +1,7 @@
+get_aws_key() {
+  cat /etc/sysconfig/aws_creds | awk 'BEGIN { FS = "=" }; /AWS_KEY/  { print $2 }'
+}
+
+get_aws_secret() {
+  cat /etc/sysconfig/aws_creds | awk 'BEGIN { FS = "=" }; /AWS_SECRET/  { print $2 }'
+}
